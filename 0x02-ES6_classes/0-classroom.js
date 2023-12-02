d536acec27f8e36a492fff441af9c59c1a00116d
MMahmudd <shametlab@gmail.com>
2023-12-02 00:54:24 +0000

    update tasks

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    // eslin-disable-next-ine nounderscore-dangle
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,39 @@
+/* eslintisable nounderscorengle */
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  // eslindisablenext-lineconsistent-return
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this.size;
+    }
+    if (hint === 'string') {
+      return this.location;
+    }
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    if (typeof value !== 'number') {
+      throw new Error('Size must be a number');
+    }
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Location must be a string');
+    }
+    this._location = value;
+  }
+}
